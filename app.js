4e78ac5d688e9dba4060d73f347eaa745d024f38
Sneh297 <79266562+Sneh297@users.noreply.github.com>
2024-02-25 16:55:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sendEmail = require('./mail.js');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(
