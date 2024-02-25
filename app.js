974725ec112026c31f7d47417a054eb50e1967bd
Sneh297 <79266562+Sneh297@users.noreply.github.com>
2024-02-25 16:41:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
-app.use(
-  cors({
-    origin: ['https://mailproject.vercel.app/'],
-    methods: ['POST', 'GET'],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ['https://mailproject.vercel.app/'],
+//     methods: ['POST', 'GET'],
+//     credentials: true,
+//   })
+// );
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static('public'));
